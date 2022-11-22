6cb1569cbb5c71342a86ca0f4f94aa9ae0877900
DESKTOP-QACT1EM\admin <milan.miscevic@imoradar24.ro>
2022-11-22 20:52:26 +0100

    Implemented CarDealer.WebApi and CarController

---
+++ CarDealer.WebApi/Controllers/CarController.cs
@@ -0,0 +1,38 @@
+﻿using CarDealer.Services.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CarDealer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CarController : ControllerBase
+    {
+        private readonly ICarService _carService;
+
+        public CarController(ICarService carService)
+        {
+            _carService = carService;
+        }
+
+        [HttpGet]
+        [Route("Car")]
+        public async Task<IActionResult> GetCar(string brand, string model, string offer)
+        {
+            if (string.IsNullOrEmpty(brand) || string.IsNullOrEmpty(model) || string.IsNullOrEmpty(offer))
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, "Please provide details: brand, model and offer.");
+            }
+
+            var car = await _carService.GetCarAsync(brand, model, offer);
+
+            if (car == null)
+            {
+                return NotFound("Car not found.");
+            }
+            else
+            {
+                return Ok(car);
+            }
+        }
+    }
+}
